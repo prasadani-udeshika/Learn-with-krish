475d519681ca086620eef5385f485d02fb0c3f3d
Prasadi_Sakalasuriya <53714333+prasadani-udeshika@users.noreply.github.com>
2022-07-19 22:27:34 +0530

    TCP Demo Server and Client

---
+++ tcp_3way-handshake_demo/server.js
@@ -0,0 +1,22 @@
+var net = require('net');
+
+var port = 42181;
+var host = '127.0.0.1';
+
+var server = net.createServer((socket) => {
+    socket.on('end', () => {
+        console.log('Server: Client Disconnected');
+    });
+});
+
+server.on('connection', (socket) => {
+    console.log(`Connected from: ${socket.remoteAddress}:${socket.remotePort}`);
+    socket.write('Hello Client');
+    socket.end;
+});
+
+server.on('error', (err) => {
+    throw err;
+});
+
+server.listen(port, host);
